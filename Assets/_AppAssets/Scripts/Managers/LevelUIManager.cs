0f0cdf93e7094dc6cc93648a45a097aca3234920
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-07-06 23:40:11 +0200

    bendary
    finish get resources to ui

--- Assets/_AppAssets/Scripts/Managers/LevelUIManager.cs
+++ Assets/_AppAssets/Scripts/Managers/LevelUIManager.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class LevelUIManager : UIManager
 {
+    #region Singleton
+    public static LevelUIManager Instance;
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
     public Color GoodColor;
     public Color MiddleColor;
     public Color BadColor;
